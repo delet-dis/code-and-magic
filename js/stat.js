b6c66f9c49733c86c08bdf425ebc9d09f2368012
Igor <efimov.036@gmail.com>
2020-07-19 18:52:18 +0700

    added notification background drawing

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,16 @@
 'use strict';
-window.renderStatistics = (ctx,names,times) =>{
 
+let renderCloud = (ctx, x, y, color) => {
+  let CLOUD_WIDTH = 500;
+  let CLOUD_HEIGHT = 200;
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT)
+};
+
+window.renderStatistics = (ctx, names, times) => {
+  //статистика тень
+  renderCloud(ctx, 110, 60, 'rgba(0,0,0,0.7)');
+
+  //статистика фронт
+  renderCloud(ctx, 100, 50, '#fff');
 };
