7acdaef029485a608f70d1dd16b8c1975e88937c
Igor <efimov.036@gmail.com>
2020-07-19 19:43:54 +0700

    started rendering diagram

--- js/stat.js
+++ js/stat.js
@@ -7,6 +7,19 @@ let renderCloud = (ctx, x, y, color) => {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT)
 };
 
+let renderDiagram = (ctx, names, times) => {
+  let minTimeElement = Math.max(...times);
+  let minTimeIndex = times.indexOf(minTimeElement);
+  let minTimeName = names[minTimeIndex];
+
+  for (const name of names) {
+    ctx.fillText(name, names.indexOf(name) * 10, 240);
+    console.log(name,names.indexOf(name)*10);
+    ctx.font = '16px PT Mono';
+    ctx.fillStyle = '#000';
+  }
+}
+
 window.renderStatistics = (ctx, names, times) => {
   //статистика тень
   renderCloud(ctx, 110, 60, 'rgba(0,0,0,0.7)');
@@ -17,12 +30,11 @@ window.renderStatistics = (ctx, names, times) => {
   //отрисовка текста
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура, Вы победили!', 110,75);
-  ctx.fillText('Список результатов:', 110,95);
-  console.log(names);
-  console.log(times);
+  ctx.fillText('Ура, Вы победили!', 110, 75);
+  ctx.fillText('Список результатов:', 110, 95);
 
-  let maxTimeElement = Math.max(...times);
-  let maxTimeIndex = times.indexOf(maxTimeElement);
-  console.log(maxTimeElement, maxTimeIndex);
+  //поиск минимального элемента и его индекса
+
+
+  renderDiagram(ctx, names, times);
 };
