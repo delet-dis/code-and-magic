d77cbe3e5294fb7c7bb142401ea38c8dddd78301
Igor <efimov.036@gmail.com>
2020-07-19 20:16:20 +0700

    code beutification

--- js/stat.js
+++ js/stat.js
@@ -8,30 +8,39 @@ let renderCloud = (ctx, x, y, color) => {
 };
 
 let renderDiagram = (ctx, names, times) => {
+
   //константы
   let COLUMN_WIDTH = 50;
+
   //вычисление максимального значения и имени
   let minTimeElement = Math.max(...times);
   let minTimeIndex = times.indexOf(minTimeElement);
   let minTimeName = names[minTimeIndex];
+
   //отрисовка
   for (const name of names) {
+
+    //позиционирование
     let nameX = 135 + names.indexOf(name) * 90;
+
     //отрисовка имен
     ctx.fillStyle = '#000';
     ctx.font = '16px PT Mono';
     ctx.fillText(name, nameX, 240);
-    //отрисовка столбцов
+    
+    //заливка столбцов
     if (name === 'Вы') {
       ctx.fillStyle = 'rgba(255,0,0,1)';
     } else {
       ctx.fillStyle = 'rgba(0,0,255,' + Math.random() + ')';
     }
+    //отрисовка столбцов
     ctx.fillRect(nameX, 195, COLUMN_WIDTH, 20);
   }
 }
 
 window.renderStatistics = (ctx, names, times) => {
+
   //статистика тень
   renderCloud(ctx, 110, 60, 'rgba(0,0,0,0.7)');
 
@@ -44,8 +53,6 @@ window.renderStatistics = (ctx, names, times) => {
   ctx.fillText('Ура, Вы победили!', 110, 75);
   ctx.fillText('Список результатов:', 110, 95);
 
-  //поиск минимального элемента и его индекса
-
-
+  //вызов рендера диаграммы
   renderDiagram(ctx, names, times);
 };
