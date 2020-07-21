faeea7c425207506b98462cb4cefbb1c91ee169f
Igor <efimov.036@gmail.com>
2020-07-21 13:14:29 +0700

    done second homework:display randomly-generated players

--- js/setup.js
+++ js/setup.js
@@ -22,7 +22,7 @@ let arrayRandElement = arr => {
 function randReturn() {
   //данные для случайных персонажей
   let playersNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  let playersSurNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирваинг'];
+  let playersSurNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   let playersCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   let playersEyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
@@ -34,7 +34,28 @@ function randReturn() {
     randPlayersData.eyesColor = arrayRandElement(playersEyesColors);
     randMassive.push(randPlayersData);
   };
-  console.log(randMassive);
+  return randMassive;
 };
 
-randReturn();
+//функция создания ДОМ элемента
+function createDOM(dataObj){
+  let wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = dataObj.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = dataObj.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = dataObj.eyesColor;
+
+  similarListElement.appendChild(wizardElement);
+};
+
+
+//функция сборки и отображения похожих персонажей + отображение самого блока
+(function displaySimilar(){
+  let randReturnResult = randReturn();
+  for (let i = 0; i<4; i++){
+    createDOM(randReturnResult[i]);
+  };
+
+  let setupSimilar = document.querySelector('.setup-similar');
+  setupSimilar.classList.remove('hidden');
+}());
