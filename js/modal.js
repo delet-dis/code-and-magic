6d5e34f71810d390d6b5055c5a850de91ae1ff12
Igor <efimov.036@gmail.com>
2020-07-23 16:54:34 +0700

    added modal close

--- js/modal.js
+++ js/modal.js
@@ -2,13 +2,22 @@
 
 //поиск модальных окон и элементов взаимодействия с ними
 const setup = document.querySelector('.setup'),
-      setupOpen = document.querySelector('.setup-open');
+  setupOpen = document.querySelector('.setup-open');
 
-let closeModal = evt => {
-
+//функция закрытия модалок
+const closeModal = evt => {
+  const target = evt.target;
+  if (target.closest('.setup-close') || evt.code === 'Escape') {
+    console.log('buh');
+    setup.classList.add('hidden');
+  }
 };
 
 //слушатель открытия модального окна
-setupOpen.addEventListener('click', function(){
+setupOpen.addEventListener('click', function () {
   setup.classList.remove('hidden');
+  document.addEventListener('keydown', closeModal);
 });
+
+//слушатель закрытия модального окна
+setup.addEventListener('click', closeModal);
