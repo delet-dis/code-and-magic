5cf97040f034621184a3f134b128e4cbb24a6286
Igor <efimov.036@gmail.com>
2020-07-23 20:38:32 +0700

    solved closing modal during aactive input

--- js/modal.js
+++ js/modal.js
@@ -2,22 +2,22 @@
 
 //поиск модальных окон и элементов взаимодействия с ними
 const setup = document.querySelector('.setup'),
-  setupOpen = document.querySelector('.setup-open');
+  setupOpen = document.querySelector('.setup-open'),
+  usernameInput = document.querySelector('.setup-user-name');
 
 //функция закрытия модалок
 const closeModal = evt => {
   const target = evt.target;
-  if (target.closest('.setup-close') || evt.code === 'Escape') {
+  let curElement = document.activeElement.toString();
+  if (target.closest('.setup-close') || evt.code === 'Escape' && curElement.includes('Input')===false) {
     document.removeEventListener('keydown', closeModal);
     setup.classList.add('hidden');
-  }
+  };
 };
-
 //слушатель открытия модального окна
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('hidden');
   document.addEventListener('keydown', closeModal);
 });
-
 //слушатель закрытия модального окна
 setup.addEventListener('click', closeModal);
