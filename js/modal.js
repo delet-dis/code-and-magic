83bdc79588b5f0c3d4d7344effb28895ca2fab97
Igor <efimov.036@gmail.com>
2020-07-30 21:04:37 +0700

    done draggable modal

--- js/modal.js
+++ js/modal.js
@@ -5,7 +5,7 @@ const setup = document.querySelector('.setup'),
   setupOpen = document.querySelector('.setup-open'),
   usernameInput = document.querySelector('.setup-user-name'),
   setupOpenIcon = document.querySelector('.setup-open-icon'),
-  dialogHandle = document.querySelector('.upload');
+  dialogHandler = setup.querySelector('.upload');
 
 //функция закрытия модалок
 const closeModal = evt => {
@@ -38,18 +38,20 @@ setupOpen.addEventListener('click', function () {
 });
 
 //перетаскивание модального окна за аватарку в несколько этапов
-dialogHandle.addEventListener('mousedown', function (evt) {
+dialogHandler.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
-  console.log('move');
 
   let startCoords = {
     x: evt.clientX,
     y: evt.clientY
   };
+
+  let dragged = false;
+
   //функция высчитывания координат
   const onMouseMove = moveEvt => {
     moveEvt.preventDefault();
-
+    dragged = true;
     let shift = {
       x: startCoords.x - moveEvt.clientX,
       y: startCoords.y - moveEvt.clientY
@@ -62,9 +64,25 @@ dialogHandle.addEventListener('mousedown', function (evt) {
 
     setup.style.top = (setup.offsetTop - shift.y) + 'px';
     setup.style.left = (setup.offsetLeft - shift.x) + 'px';
-};
+  };
+
+  //функция отписки от обработчика события
+  const onMouseUp = upEvt => {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if(dragged){
+      const onClickPreventDefault = evt =>{
+        evt.preventDefault();
+        dialogHandler.removeEventListener('click', onClickPreventDefault);
+      };
+      dialogHandler.addEventListener('click', onClickPreventDefault);
+    }
+  };
   document.addEventListener('mousemove', onMouseMove);
-  // document.addEventListener('mouseup', onMouseUp);
+  document.addEventListener('mouseup', onMouseUp);
 });
 
 
