e6f9f653335bacd8fe84aded70a5544342403ce1
Igor <efimov.036@gmail.com>
2020-08-18 09:40:26 +0300

    added avatar changing

---
+++ js/avatar.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//функция изменения превью аватарки польщователя
+(function () {
+  const filePicker = document.querySelector('.upload input[type=file]'),
+    preview = document.querySelector('.setup-user-pic'),
+    previewNonModal = document.querySelector('.setup-open-icon');
+
+  const FILE_TYPES = ['gif', 'jpeg', 'png', 'jpg'];
+
+  //слушатель изменения аватарки
+  filePicker.addEventListener('change', () => {
+    let file = filePicker.files[0];
+
+    let fileName = file.name.toLowerCase();
+
+    let matches = FILE_TYPES.some((item) => {
+      return fileName.endsWith(item);
+    });
+
+    if (matches) {
+      let reader = new FileReader();
+
+      reader.addEventListener('load', () => {
+        preview.src = reader.result;
+        previewNonModal.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  })
+})();
